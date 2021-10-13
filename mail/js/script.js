f351233ac6db96a82dc9f4402728af6cc8584baa
Frameck <fra9879@gmail.com>
2021-10-13 17:16:53 +0200

    Completed exercise - mail

---
+++ mail/js/script.js
@@ -0,0 +1,28 @@
+const validMails = ['ultron@gmail.com', 'thanos@gmail.com', 'spiderman@gmail.com', 'starlord@gmail.com']
+const userMail = prompt('Inserisci il tuo indirizzo mail')
+
+let message = ''
+let canAccess = false   // global variable to check if the user can access or not
+
+for (let i = 0; i < validMails.length; i++) {
+    const currentMail = validMails[i];
+    if (userMail === currentMail) {
+        canAccess = true
+    }
+}
+
+if (canAccess) {
+    message = 'Puoi accedere'
+} else {
+    message = 'Ci dispiace, non è consentito l\'accesso con questo indirizzo mail'
+}
+
+alert(message)
+
+
+// adding the results to the DOM
+const container = document.querySelector('.container')
+const results = document.createElement('div')
+results.textContent = message
+
+container.appendChild(results)
